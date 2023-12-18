7fa52c30e961faaf8d1926ce53a2635f7e510477
JDiegoOnyx <juandiegog.onyx@gmail.com>
2023-12-18 15:19:07 -0500

    new charttype and logic

--- barranquilla/src/components/DashCardDatos/DashCardDatos.js
+++ barranquilla/src/components/DashCardDatos/DashCardDatos.js
@@ -1,40 +1,54 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import '../../components/main.css';
 import ImagenUser from '../../assets/img/Imagen-prueba.jpg';
-import { XYPlot, LineMarkSeries, XAxis, YAxis } from 'react-vis';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+
+const data = [
+  { name: 'Page A', uv: 400, pv: 2400, amt: 2400 },
+  { name: 'Page B', uv: 300, pv: 4567, amt: 2400 },
+  { name: 'Page C', uv: 200, pv: 1398, amt: 2400 },
+  { name: 'Page D', uv: 278, pv: 9800, amt: 2400 },
+  { name: 'Page E', uv: 189, pv: 3908, amt: 2400 },
+  { name: 'Page F', uv: 239, pv: 4800, amt: 2400 },
+  { name: 'Page G', uv: 349, pv: 3800, amt: 2400 },
+];
 
 function DashCardDatos() {
-  const data = [
-    { x: 0, y: 8 },
-    { x: 1, y: 5 },
-    { x: 2, y: 4 },
-    { x: 3, y: 9 },
-    { x: 4, y: 1 },
-    { x: 5, y: 7 },
-    { x: 6, y: 6 },
-    { x: 7, y: 3 },
-    { x: 8, y: 2 },
-    { x: 9, y: 0 }
-  ];
-  
+  const chartContainerRef = useRef(null);
+  const [chartWidth, setChartWidth] = useState(0);
+
+  useEffect(() => {
+    const updateChartWidth = () => {
+      if (chartContainerRef.current) {
+        const width = chartContainerRef.current.clientWidth;
+        setChartWidth(width);
+      }
+    };
+
+    // Update width initially and on window resize
+    updateChartWidth();
+    window.addEventListener('resize', updateChartWidth);
+
+    return () => {
+      window.removeEventListener('resize', updateChartWidth);
+    };
+  }, []);
+
   return (
     <div className=''>
-      <div className="dash-card-datos">
+      <div ref={chartContainerRef} className="dash-card-datos"> {/* Contenedor padre */}
         <div className="dash-datos-header">
           <img src={ImagenUser} className="dash-header-image" alt="User" />
           <div className="dash-card-overlay"></div>
           <p className='dash-datos-title'><i>Datos y estadísticas con <strong>machine learning e inteligencia artificial</strong></i></p>
         </div>
-        <div className="">
-        <XYPlot width={1400} height={300}>
-          <LineMarkSeries
-            data={data}
-            style={{ strokeLinejoin: "round" }}
-          />
-          <XAxis style={{ line: { stroke: 'white' }, ticks: { stroke: 'white' }, text: { fill: 'white' } }} />
-          <YAxis style={{ line: { stroke: 'white' }, ticks: { stroke: 'white' }, text: { fill: 'white' } }} />
-        </XYPlot>
-        </div>
+        <LineChart width={chartWidth} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+          <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+        </LineChart>
       </div>
     </div>
   );
